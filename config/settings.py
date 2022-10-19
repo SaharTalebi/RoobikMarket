35e22718e770bc55f19d0ef3ec19d7a103424930
sahar <s.talebi.sut@gmail.com>
2022-10-19 20:02:12 +0330

    create userprofile app

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from environs import Env
+
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c7=+cq#1exktw2p+$%1rvnm-g0#x$a(pgqqp%s1)@5!0qn3w6_'
+SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DJANGO_DEBUG")
 
 ALLOWED_HOSTS = []
 
@@ -53,6 +57,7 @@ INSTALLED_APPS = [
     'blog',
     'product',
     'cart',
+    'userprofile',
 ]
 
 MIDDLEWARE = [
